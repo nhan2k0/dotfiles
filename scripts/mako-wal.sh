081ea04a79e9d28bcc5e91c15011748909e59990
Hiep Tran <164328857+htran2isnotadev@users.noreply.github.com>
2024-10-19 20:17:31 +0700

    Update mako-wal.sh

--- scripts/mako-wal.sh
+++ scripts/mako-wal.sh
@@ -2,7 +2,7 @@
 xrdb ~/.cache/wal/colors.Xresources && xrdb -merge ~/.cache/wal/colors.Xresources
 hyprctl reload
 cp ~/.cache/wal/colors-waybar.css ~/.config/waybar && pkill -USR2 waybar
-spicetify refresh # in spotify hit ctrl+shift+r to actually refresh the colors
+# spicetify refresh # only needed if you have already enabled devtools. spicetify required.
 cp ~/.cache/wal/config ~/.config/cava && pkill -USR2 cava
 cp ~/.cache/wal/starship.toml ~/.config/starship/
 cp ~/.cache/wal/colors-swaync.css ~/.config/swaync/ && swaync-client -R && swaync-client -rs
