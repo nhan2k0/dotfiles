1f793c9087693a6b84c28b95ea7da455b30987c7
Hiep Tran <164328857+htran2isnotadev@users.noreply.github.com>
2024-10-19 20:16:08 +0700

    Add files via upload

---
+++ scripts/mako-wal.sh
@@ -0,0 +1,8 @@
+#apply the new scheme
+xrdb ~/.cache/wal/colors.Xresources && xrdb -merge ~/.cache/wal/colors.Xresources
+hyprctl reload
+cp ~/.cache/wal/colors-waybar.css ~/.config/waybar && pkill -USR2 waybar
+spicetify refresh # in spotify hit ctrl+shift+r to actually refresh the colors
+cp ~/.cache/wal/config ~/.config/cava && pkill -USR2 cava
+cp ~/.cache/wal/starship.toml ~/.config/starship/
+cp ~/.cache/wal/colors-swaync.css ~/.config/swaync/ && swaync-client -R && swaync-client -rs
